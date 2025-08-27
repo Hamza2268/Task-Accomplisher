36fbe2be231359dd576fcbd7f146d4e179257b85
SeifEldeinHazem <seifeldeinhazem8@gmail.com>
2025-08-28 01:29:22 +0300

    'Almost done udpating user info, password ,tasks info, profile pic and some front styling'

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-# from click import style
-# from email.policy import default
-from calendar import c
-from flask import Flask, redirect, render_template, url_for, session
+######################################################### Used modules #############################################################
+from flask import Flask, redirect, render_template, url_for,request ,session
 from flask_wtf import FlaskForm
-from sqlalchemy import Null
+from sqlalchemy import Null, nullsfirst
 from wtforms import StringField, PasswordField, SubmitField, RadioField, FileField, SelectField
 from wtforms.validators import DataRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
@@ -13,9 +11,9 @@ from werkzeug.utils import secure_filename
 import uuid
 from flask_bcrypt import Bcrypt
 from flask_wtf.file import FileAllowed
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
  
-
+####################################################### App Initialization #####################################################
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
@@ -28,13 +26,13 @@ app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 if __name__ == '__main__':
     app.run(debug=True)
 
-### DB Configuration
+####################################################### DB Configuration #####################################################
 app.config['SECRET_KEY'] = 'your_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
+######################################################### Forms #############################################################
 ##### Registeration Form
 class RegisterForm(FlaskForm):
     UserName = StringField('UserName', validators=[DataRequired(), Length(min=3, max=20)])
@@ -53,6 +51,22 @@ class LoginForm(FlaskForm):
     Password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     Submit = SubmitField('Login')
 
+##### Personal-Info Form
+class PersonalInfoForm(FlaskForm):
+    UserName = StringField('UserName', validators=[DataRequired(), Length(min=3, max=20)])
+    Email = StringField('Email', validators=[DataRequired(), Email()])
+    photo = FileField("Profile Picture", validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
+    Address = StringField('Address', validators=[DataRequired(), Length(max=100)])
+    Phone = StringField('Phone', validators=[DataRequired(), Length(min=10, max=15)])
+    Submit = SubmitField('Update Info')
+
+##### Change Password Form
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Current Password', validators=[DataRequired(), Length(min=8)])
+    new_password = PasswordField('New Password', validators=[DataRequired(), Length(min=8)])
+    confirm_new_password = PasswordField('Confirm New Password', validators=[DataRequired(), Length(min=8)])
+    Submit = SubmitField('Change Password')
+
 class TaskForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(min=3, max=100)])
     description = StringField('Description', validators=[Length(max=200)])
@@ -68,7 +82,7 @@ class TaskForm(FlaskForm):
                                validators=[DataRequired()])
     Submit = SubmitField('Add Task')
 
-##################################### Models #########################################
+######################################################### Models #############################################################
 ### DB Model
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -99,17 +113,16 @@ class Task(db.Model):
     def __repr__(self):
         return f"Task('{self.title}', '{self.description}')"
 
-##################################### Routes #########################################
 
-# ## current user id
-# id = Null 
-
+######################################################### Routes #############################################################
+### HomePage for the Web info
 @app.route('/')
 def homepage():
     global id
     id = Null
     return render_template('homepage.html')
 
+### Registeration route
 @app.route('/app/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
@@ -145,36 +158,46 @@ def register():
         return redirect(url_for('Registered_successfully'))
     return render_template('register.html', form=form, title='Registeration Form', style='register_signin.css', script='script.js')
 
+###################################################### app Path ########################################################
+### Successful Registeration msg
 @app.route('/app/successful-process')
 def Registered_successfully():
     return render_template('successful.html', title='Registered Successfully', style='register_signin.css')
 
+### Log In route
 @app.route('/app/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    # if request.method == 'POST':
     if form.validate_on_submit():
-        # user = User.query.filter_by(email=form.Email.data, password=form.Password.data).first()
         user = User.query.filter_by(email=form.Email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.Password.data):
-            # global id
-            # id = user.id
             session['user_id'] = user.id
             return redirect(url_for('Home'))
         else:
             form.Email.errors.append("Invalid username or password")
     return render_template("login.html", form=form, title='Login', style='register_signin.css', script='script.js')
 
+###################################################### User Path ########################################################
+#User Home route
 @app.route('/user/home')
 def Home():
-    # global id
-    # if id == Null:
-    #     return redirect(url_for('login'))
     if 'user_id' not in session:
         return redirect(url_for('login'))
     user = User.query.get(session['user_id'])
     return render_template('home.html', user=user, title='Home', style='register_signin.css', script='script.js')
 
+### Tasks made by User
+@app.route('/user/task')
+def view_tasks():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    user = User.query.get(session['user_id'])
+    tasks = Task.query.filter_by(user_id=session['user_id']).order_by(nullsfirst(Task.CompletedDate)).all()
+    acheived = Task.query.filter_by(user_id=session['user_id']).filter(Task.CompletedDate.isnot(None)).count()
+    total = Task.query.filter_by(user_id=session['user_id']).count()
+    return render_template('viewTasks.html', user=user, acheived=acheived, total=total,  tasks=tasks, title='Tasks', style='register_signin.css', script='script.js')
+
+### User Personal Info
 @app.route('/user/personal-info')
 def personal_info():
     if 'user_id' not in session:
@@ -182,48 +205,106 @@ def personal_info():
     user = User.query.get(session['user_id'])
     return render_template('personal_info.html', user=user, title='Personal Info', style='register_signin.css', script='script.js')
 
-@app.route('/user/task')
-def view_tasks():
+### Edit Personal Info route
+@app.route('/user/personal-info/edit', methods=['GET','POST'])
+def Edit_Info():
     if 'user_id' not in session:
         return redirect(url_for('login'))
     user = User.query.get(session['user_id'])
-    tasks = Task.query.filter_by(user_id=session['user_id']).all()
-    return render_template('viewTasks.html', user=user, tasks=tasks, title='Tasks', style='register_signin.css', script='script.js')
+    form = PersonalInfoForm()
+    if request.method == 'GET':
+        form.UserName.data = user.username
+        form.Email.data = user.email
+        form.Address.data = user.Address
+        form.Phone.data = user.Phone
+    
+    if form.validate_on_submit():
+        if User.query.filter_by(email=form.Email.data).count() > 0 and user.email != form.Email.data:
+            form.Email.errors.append("Email already registered")
+            return render_template('editInfo.html', form=form, title='Edit Info', style='register_signin.css', script='script.js')
+        
+        photo_file = request.files.get('photo')   
+        if photo_file and photo_file.filename != "":
+            filename = secure_filename(photo_file.filename)
+            unique_filename = str(uuid.uuid4()) + "_" + filename
+            photo_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
+            photo_file.save(photo_path)
+            user.photo = unique_filename  
+        user.username = form.UserName.data
+        user.email = form.Email.data  
+        user.Address = form.Address.data
+        user.Phone = form.Phone.data
+        db.session.commit()
+        return redirect(url_for('personal_info'))
 
+    return render_template('editInfo.html', form=form, title='Edit Info', style='register_signin.css', script='script.js')
+
+### Change User Password route
+@app.route('/user/personal-info/edit/password', methods=['GET','POST'])
+def changePassword():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        if(form.new_password.data != form.confirm_new_password.data):
+            form.new_password.errors.append("Passwords do not match")
+            return render_template('editInfo.html', form=form, title='change password', style='register_signin.css', script='script.js')
+        user = User.query.get(session['user_id'])
+        if user and bcrypt.check_password_hash(user.password, form.current_password.data):
+           hashed_password = bcrypt.generate_password_hash(form.new_password.data).decode('utf-8')
+           user.password= hashed_password
+           db.session.commit()
+           return render_template('successful.html',msg='Password Changed successfully', title='Registered Successfully', style='register_signin.css')
+    return render_template('editInfo.html', form=form, title='change password', style='register_signin.css', script='script.js')
+
+###################################################### Task Path ########################################################
 ### Add New Task Route
 @app.route('/user/task/add', methods=['GET', 'POST'])
 def add_task():
     if 'user_id' not in session:
         return redirect(url_for('login'))
+    user = User.query.get(session['user_id'])
     form = TaskForm()
     if form.validate_on_submit():
         task = Task(title=form.title.data,
                     description=form.description.data,
                     user_id=session['user_id'],
                     Deadline=int(form.Deadline.data),
+                    EndDate=datetime.now(timezone.utc) + timedelta(days=int(form.Deadline.data)),
                     priority=form.priority.data)
         db.session.add(task)
         db.session.commit()
         return redirect(url_for('Home'))
-    return render_template('addTask.html', form=form, title='Add New Task', style='register_signin.css', script='script.js')
+    return render_template('add_edit_Task.html',user= user, form=form, title='Add New Task', style='register_signin.css', script='script.js')
 
-# @app.route('/user/task/edit/<int:task_id>', methods=['GET', 'POST'])
-# def edit_task(task_id):
-#     task = Task.query.get_or_404(task_id)
-#     return render_template('edit_task.html', task=task)
+### Edit Task content
+@app.route('/user/task/edit/<int:task_id>', methods=['GET','POST'])
+def edit_task(task_id):
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    task = Task.query.get_or_404(task_id)
+    if task.user_id != session['user_id']:
+        return "You are not authorized to complete this task.", 403
+    form = TaskForm()
+    user = User.query.get(session['user_id'])
+    if request.method == 'GET':
+        form.title.data = task.title
+        form.description.data = task.description
+        form.Deadline.data = str(task.Deadline)
+        form.priority.data = task.priority
 
+    if form.validate_on_submit():
+        task.title = form.title.data
+        task.description = form.description.data
+        task.Deadline = int(form.Deadline.data)
+        task.priority = form.priority.data
+        task.EndDate = datetime.now(timezone.utc) + timedelta(days=int(form.Deadline.data))
+        
+        db.session.commit()
+        return redirect(url_for('view_tasks'))
+    return render_template('add_edit_Task.html', user=user, task=task, form=form, title='Edit Task', style='register_signin.css', script='script.js')
 
-### Logout Route
-@app.route('/app/logout')
-def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('homepage'))
-
-### 404 Error Route
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('notFoundError.html', title='404 Error', style='register_signin.css', script='script.js'), 404
-
+### Deletion of Tasks route
 @app.route('/user/task/delete/<int:task_id>', methods=['GET'])
 def delete_task(task_id):
     if 'user_id' not in session:
@@ -235,7 +316,8 @@ def delete_task(task_id):
     db.session.commit()
     return redirect(url_for('view_tasks'))
 
-@app.route('/user/task/done/<int:task_id>', methods=['POST'])
+### Mark Task as Completed route
+@app.route('/user/task/done/<int:task_id>', methods=['GET', 'POST'])
 def complete_task(task_id):
     if 'user_id' not in session:
         return redirect(url_for('login'))
@@ -246,26 +328,15 @@ def complete_task(task_id):
     db.session.commit()
     return redirect(url_for('view_tasks'))
 
+### 404 Error Route
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('notFoundError.html', title='404 Error', style='register_signin.css', script='script.js'), 404
 
-@app.route('/user/task/edit/<int:task_id>', methods=['GET','POST'])
-def edit_task(task_id):
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-    task = Task.query.get_or_404(task_id)
-    if task.user_id != session['user_id']:
-        return "You are not authorized to complete this task.", 403
-    form = TaskForm()
-    if form.validate_on_submit():
-        task = Task(title=form.title.data,
-                    description=form.description.data,
-                    user_id=session['user_id'],
-                    Deadline=int(form.Deadline.data),
-                    priority=form.priority.data)
-        db.session.add(task)
-        db.session.commit()
-        return redirect(url_for('Home'))
-    return render_template('addTask.html', task=task, form=form, title='Edit Task', style='register_signin.css', script='script.js')
-
-
+### Logout Route
+@app.route('/app/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('homepage'))
 
 ### The End ###
--- static/script/script.js
+++ static/script/script.js
@@ -13,6 +13,7 @@ if (welcome) {
     }
     setInterval(typeWriter, 200)
 }
+
 // $("#right").on("click", function () {
 //     $(".block").animate({ "left": "+=50px" }, "slow");
 // });
@@ -27,13 +28,57 @@ if (descr.value.length >= 200) {
 }
 
 document.querySelectorAll(".timeline").forEach(el => {
+    if(el.classList.contains("complete") || el.classList.contains("overdue")){
+        return;
+    }
     let start = new Date(el.dataset.start);
-    let end = new Date(el.dataset.end) ;
+    let end = new Date() ;
     let diffDays = Math.floor((end - start) / (1000 * 60 * 60 * 24));
     let deadline = parseInt(el.dataset.deadline, 10);
-    console.log(diffDays, deadline);
-    if(diffDays > 0)
-        el.style.width = (diffDays / deadline) * 100 + "%"
-    else
-        el.style.width = "1%" 
+    let percent = ((diffDays / deadline) * 100);
+    console.log((start- end)/(1000 * 60 * 60 * 24));
+    console.log(diffDays, deadline, percent);
+    el.style.width = (diffDays > 0) ? (percent + "%") : "1%" ;
+    if (percent < 25) {
+        el.style.backgroundColor = "#28a745";
+    } else if (percent < 50) {
+        el.style.backgroundColor = "#ffc107"; 
+    } else if (percent < 75) {
+        el.style.backgroundColor = "#fd7e14"; 
+    } else if( percent <= 100) {
+        el.style.backgroundColor = "#dc3545"; 
+    } else {
+        el.classList.add("overdue");
+    }
+    
 });
+
+let toggle = document.querySelector("#togglePassword");
+if (toggle)
+toggle.addEventListener("click", function () {
+        let input = document.getElementById("floatingPassword");
+        if (input.type === "password") {
+            input.type = "text";
+            this.classList.remove("fa-eye");
+            this.classList.add("fa-eye-slash");
+        } else {
+            input.type = "password";
+            this.classList.remove("fa-eye-slash");
+            this.classList.add("fa-eye");
+        }
+    });
+
+function Done(id){
+    let taskCard = document.getElementById(id);
+    taskCard.classList.add("complete");
+}
+
+let achievment = document.getElementById("achievment-line");
+if(achievment){
+    let acheived = parseInt(achievment.getAttribute("acheived"));
+    let total = parseInt(achievment.getAttribute("total"));
+    let percent = (total == 0) ? 0 : Math.floor((acheived / total) * 100);
+    achievment.style.width = percent + "%";
+    achievment.classList.add("bg-success");
+
+}
